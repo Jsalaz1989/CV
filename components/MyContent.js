9bdb307c395c6ad75f6a7d5fee30ff0df4ec9319
jsalaz1989 <jsalaz1989@gmail.com>
2020-09-24 12:18:09 +0200

    Added CVs in both languages

--- components/MyContent.js
+++ components/MyContent.js
@@ -227,6 +227,7 @@ export default () => (
                     <a href='static/CV (English)- Jaime Salazar Lahera.pdf' title='CV (English) - Jaime Salazar Lahera' download='CV (English) - Jaime Salazar Lahera'>English</a> 
                     or
                     <a href='static/CV (Spanish)- Jaime Salazar Lahera.pdf' title='CV (Spanish) - Jaime Salazar Lahera' download='CV (Spanish) - Jaime Salazar Lahera'>Spanish</a>
+                or scroll down for details.
             </p>
         </div>
         <div className='entry'>
